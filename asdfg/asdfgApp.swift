9297e2e127ecc83bb47573aa110041963822ad18
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-03-15 18:55:24 +0530

    Initial Commit

---
+++ asdfg/asdfgApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdfgApp.swift
+//  asdfg
+//
+//  Created by Test Ios on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct asdfgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
